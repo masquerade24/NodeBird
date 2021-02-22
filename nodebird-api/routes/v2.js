375f48b03b894fb8b4e5710770afc79986564861
masquerade24 <ewr1029@naver.com>
2021-02-22 17:56:15 +0900

    Ch9, 10 completed

--- nodebird-api/routes/v2.js
+++ nodebird-api/routes/v2.js
@@ -1,22 +1,30 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const url = require('url');
 
-const { verifyToken, deprecated } = require('./middlewares');
+const { verifyToken, apiLimiter } = require('./middlewares');
 const { Domain, User, Post, Hashtag } = require('../models');
 
 const router = express.Router();
 
-router.use(deprecated); // v1으로 접근한 모든 요청에 deprecated 응답을 보낸다.
-
-// GET /v1/test 토큰을 테스트해볼 수 있는 라우터
-router.get('/test', verifyToken, (req, res) => {
-    res.json(req.decoded);
+router.use(async (req, res, next) => {
+    const domain = await Domain.findOne({
+        where: { host: url.parse(req.get('origin')).host },
+    });
+    if (domain) {
+        cors({
+            origin: req.get('origin'),
+            credentials: true,
+        })(req, res, next);
+    } else {
+        next();
+    }
 });
 
-// POST /v1/token 토큰을 발급하는 라우터
-router.post('/token', async (req, res) => {
+router.post('/token', apiLimiter, async (req, res) => {
     const { clientSecret } = req.body;
-    try { // 전달받은 클라이언트 비밀키로 도메인이 등록된 것인지 확인한다.
+    try {
         const domain = await Domain.findOne({
             where: { clientSecret },
             include: {
@@ -24,17 +32,17 @@ router.post('/token', async (req, res) => {
                 attribute: ['nick', 'id'],
             },
         });
-        if (!domain) { // 도메인이 등록된 것이 아니라면
+        if (!domain) {
             return res.status(401).json({
                 code: 401,
-                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요.',
             });
-        } // 도메인이 등록된 것이라면 jwt.sign으로 토큰을 발급받는다. 
-        const token = jwt.sign({ // jwt.sign(토큰의 내용, 비밀키, 토큰의 설정)
+        }
+        const token = jwt.sign({
             id: domain.User.id,
             nick: domain.User.nick,
         }, process.env.JWT_SECRET, {
-            expiresIn: '1m', // 1분
+            expiresIn: '30m', // 30분
             issuer: 'nodebird',
         });
         return res.json({
@@ -51,8 +59,11 @@ router.post('/token', async (req, res) => {
     }
 });
 
-// GET /posts/my 라우터. 내가 올린 게시글을 가져온다.
-router.get('/posts/my', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
+    res.json(req.decoded);
+});
+
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
     Post.findAll({ where: { userId: req.decoded.id } })
         .then((posts) => {
             console.log(posts);
@@ -70,8 +81,7 @@ router.get('/posts/my', verifyToken, (req, res) => {
         });
 });
 
-//GET /posts/hashtag/:title 라우터. 해시태그 검색 결과를 가져온다.
-router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
+router.get('/posts/hashtag/:title', verifyToken, apiLimiter, async (req, res) => {
     try {
         const hashtag = await Hashtag.findOne({ where: { title: req.params.title } });
         if (!hashtag) {
@@ -89,7 +99,7 @@ router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
         console.error(error);
         return res.status(500).json({
             code: 500,
-            message: '서버 에러',
+            message: '서버 에러'
         });
     }
 });
--- nodecat/routes/index.js
+++ nodecat/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const axios = require('axios');
 
 const router = express.Router();
-const URL = 'http://localhost:8002/v1';
+const URL = 'http://localhost:8002/v2';
+
 axios.defaults.headers.origin = 'http://localhost:4000'; // origin 헤더 추가
 const request = async (req, api) => {
     try {
@@ -12,14 +13,14 @@ const request = async (req, api) => {
             });
             req.session.jwt = tokenResult.data.token; // 세션에 토큰 저장
         }
-        return await axios.genpmt(`${URL}${api}`, {
+        return await axios.get(`${URL}${api}`, {
             headers: { authorization: req.session.jwt },
         }); // API 요청
     } catch (error) {
-        if (error.response.status === 419) { // 토큰 만료 시 토큰 재발급 받기
+        if (error.response.status === 419) { // 토큰 만료시 토큰 재발급 받기
             delete req.session.jwt;
             return request(req, api);
-        } // 419 외의 다른 에러라면
+        } // 419 외의 다른 에러면
         return error.response;
     }
 };
@@ -46,6 +47,10 @@ router.get('/search/:hashtag', async (req, res, next) => {
             next(error);
         }
     }
+});
+
+router.get('/', (req, res) => {
+    res.render('main', { key: process.env.CLIENT_SECRET });
 });
 
 module.exports = router;
